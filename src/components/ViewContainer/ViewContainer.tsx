5505e2700a546fcf1d199aff34f0a36c3419f495
Intrstng <intrstg@gmail.com>
2024-07-05 19:32:50 +0300

    style: add position to App sections

--- src/components/ViewContainer/ViewContainer.tsx
+++ src/components/ViewContainer/ViewContainer.tsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { ViewContainerProps } from '../../types/types';
+import S from './ViewContainer.module.css';
 
 export class ViewContainer extends Component<ViewContainerProps> {
   render() {
     return (
-      <section>
+      <section className={S.viewContainer}>
         <p>Hello!</p>
       </section>
     );
