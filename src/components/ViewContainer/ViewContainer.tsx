8d23c159dbb3ef4e35838eae05b983672f6b63aa
Intrstng <intrstg@gmail.com>
2024-07-09 11:50:24 +0300

    refactor: change ViewContainer class component to functional component

--- src/components/ViewContainer/ViewContainer.tsx
+++ src/components/ViewContainer/ViewContainer.tsx
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import { ViewContainerProps } from '../../types/types';
 import S from './ViewContainer.module.css';
 import { Vehicle } from '../Vehicle/Vehicle';
 
-export class ViewContainer extends Component<ViewContainerProps, unknown> {
-  render() {
-    return (
-      <section className={S.viewContainer}>
-        {this.props.vehicles.length > 0 ? (
-          <ul className={S.vehiclesList}>
-            {this.props.vehicles.map((vehicle, idx) => (
-              <li key={idx}>
-                <Vehicle vehicle={vehicle} />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <h2 className={S.notification}>
-            No results were found for your request...
-          </h2>
-        )}
-      </section>
-    );
-  }
-}
+export const ViewContainer: FC<ViewContainerProps> = ({ vehicles }) => {
+  return (
+    <section className={S.viewContainer}>
+      {vehicles.length > 0 ? (
+        <ul className={S.vehiclesList}>
+          {vehicles.map((vehicle, idx) => (
+            <li key={idx}>
+              <Vehicle vehicle={vehicle} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <h2 className={S.notification}>
+          No results were found for your request...
+        </h2>
+      )}
+    </section>
+  );
+};
