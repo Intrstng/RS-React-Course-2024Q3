dfbf956913869aa62a3d33755dff576dce62dc8f
Intrstng <intrstg@gmail.com>
2024-08-14 23:38:11 +0300

    feat: add Prettier, Husky, RTK, React-Router, Yup, React hook form

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,47 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.{ts,tsx}'],
-  ignores: ['dist'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-  },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    ignores: ["dist",
+        "node_modules",
+        "eslint.config.js",
+        "*.js",
+        "*.jsx",
+        ".vscode",
+        ".idea",
+        ".gitignore",
+        ".prettierrc",
+        ".prettierignore",
+        "package.json",
+        "package-lock.json",
+        "tsconfig.app.json",
+        "tsconfig.node.json",
+        ".husky",
+        "tsconfig.json",
+        "vite.config.ts",
+        "README.md"
     ],
-    '@typescript-eslint/no-explicit-any': 'error',
-    'no-undef': 'error',
-    '@typescript-eslint/no-unused-vars': 'off'
-  },
-})
+    languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+    },
+    plugins: {
+        "react-hooks": reactHooks,
+        "react-refresh": reactRefresh,
+    },
+    rules: {
+        ...reactHooks.configs.recommended.rules,
+        "react-refresh/only-export-components": [
+            "warn",
+            {allowConstantExport: true},
+        ],
+        "@typescript-eslint/no-explicit-any": "error",
+        "no-undef": "error",
+        "@typescript-eslint/no-unused-vars": "off",
+    },
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-const a = 2
+  const [count, setCount] = useState(0);
+
   const sum = (a: number, b: number): void => {
-    console.log(a + b)
-  }
-  sum(2, 2)
+    console.log(a + b);
+  };
+  sum(2, 2);
 
   return (
     <>
@@ -34,7 +34,7 @@ const a = 2
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
